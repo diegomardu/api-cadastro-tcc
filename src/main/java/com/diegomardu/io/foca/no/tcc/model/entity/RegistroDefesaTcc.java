442dcbaf5fc769bbb30f5b917a3dc5e5f164f0a0
Diego Martins Duarte <diego.martins.duarte@accenture.com>
2022-07-23 21:27:05 -0300

    Implementado model,repository e controller para registro de defesa

---
+++ src/main/java/com/diegomardu/io/foca/no/tcc/model/entity/RegistroDefesaTcc.java
@@ -0,0 +1,32 @@
+package com.diegomardu.io.foca.no.tcc.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+public class RegistroDefesaTcc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "id_aluno")
+    private Aluno aluno;
+
+    @OneToOne
+    @JoinColumn(name = "id_professor")
+    private Professor professor;
+
+    private Date data;
+
+    private String periodo_letivo;
+
+    @OneToOne
+    @JoinColumn(name = "id_tcc")
+    private TrabalhoConclusaoCurso trabalhoConclusaoCurso;
+
+}
